3ede490783c8c48d98d071aa75ccf8e993e68ae2
Scott Nemes <scottmnemes@gmail.com>
2024-07-20 10:36:30 -0700

    Added main logic

--- tabapay_status.py
+++ tabapay_status.py
@@ -3,20 +3,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-status_page = requests.get("https://developers.tabapay.net/status.html")
+def main():
+    status_page = requests.get("https://developers.tabapay.net/status.html")
 
-soup = BeautifulSoup(status_page.content, "html.parser")
+    soup = BeautifulSoup(status_page.content, "html.parser")
 
-status_table = soup.find("table", class_="tr")
-headings = status_table.find_all("th", string=lambda text: text != "Environment")
-statuses = status_table.find_all("td", class_="n1")
+    status_table = soup.find("table", class_="tr")
+    headings = status_table.find_all("th", string=lambda text: text != "Environment")
+    statuses = status_table.find_all("td", class_="n1")
 
-header_list = [heading.text.strip() for heading in headings ]
-status_list = [status.text.strip().replace("✔ ", "") for status in statuses]
+    header_list = [heading.text.strip() for heading in headings ]
+    status_list = [status.text.strip().replace("✔ ", "") for status in statuses]
 
-service_status = {}
+    service_status = {}
 
-for k, v in enumerate(header_list):
-    service_status[v] = status_list[k]
+    for k, v in enumerate(header_list):
+        service_status[v] = status_list[k]
 
-print(service_status)
+    print(service_status)
+
+if __name__ == "__main__":
+    main()
